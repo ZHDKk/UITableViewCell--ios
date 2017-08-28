15bdc3e72b26fe8ac95837771fa334087a2c52b1
zh dk <zhdk@zhdeMacBook-Pro.local>
2017-08-28 15:33:43 +0800

    Initial Commit

---
+++ UITableView高级协议/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableView高级协议
+//
+//  Created by zh dk on 2017/8/28.
+//  Copyright © 2017年 zh dk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableView高级协议/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableView高级协议
+//
+//  Created by zh dk on 2017/8/28.
+//  Copyright © 2017年 zh dk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
